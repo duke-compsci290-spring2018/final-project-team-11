6817ef0d4ff01534e7d27bf5f94b12739fcd61e4
Martin Muenster <mm617@duke.edu>
2018-04-29 13:37:55 -0400

    ejrnv

--- athena/src/components/Navigation.js
+++ athena/src/components/Navigation.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Toolbar from './Toolbar.js'
 
@@ -27,20 +26,3 @@ export default class Navigation extends React.Component {
 
     }
 }
-=======
-import { Link } from 'react-router-dom';
-
-import * as routes from '../constants/routes';
-
-const Navigation = () =>
-    <div>
-        <ul>
-            <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
-            <li><Link to={routes.LANDING}>Landing</Link></li>
-            <li><Link to={routes.HOME}>Home</Link></li>
-            <li><Link to={routes.ACCOUNT}>Account</Link></li>
-        </ul>
-    </div>
-
-export default Navigation
->>>>>>> 3e9cd89fd85fba9abc2965b88a815f295f977121
